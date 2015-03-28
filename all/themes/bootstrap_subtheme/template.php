56100ffea6c7dfd4b733fb51360c62e101d83632
plsg <patrick@patricklesage.ca>
2015-03-27 23:14:08 -0400

    disable register button

--- all/themes/bootstrap_subtheme/template.php
+++ all/themes/bootstrap_subtheme/template.php
@@ -51,3 +51,64 @@ function bootstrap_subtheme_date_nav_title($params) {
 
 
 
+/**
+ * Theme function for the modal.
+ */
+function bootstrap_subtheme_bootstrap_login_modal_output($vars) {
+  $links = '<ul class="menu nav navbar-nav pull-right"><li><a href="#" data-toggle="modal" data-target="#login-modal">' . t('Login') . '</a></li>';
+  // Login modal.
+  $login_form = drupal_get_form('user_login');
+  $login_modal = '
+      <div class="modal fade" id="login-modal" role="dialog" aria-labelledby="modalLoginLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">' . t('Close') . '</span></button>
+              <h4 class="modal-title" id="modalLoginLabel">' . t('Login') . '</h4>
+            </div>
+      <div class="modal-body">
+      ' . drupal_render($login_form) . '
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">' . t('Close') . '</button>
+      </div>
+          </div>
+        </div>
+      </div>
+      ';
+  // Register modal and link.
+  $register_modal = '';
+  if (variable_get('user_register', USER_REGISTER_VISITORS_ADMINISTRATIVE_APPROVAL)) {
+    $register_form = drupal_get_form('user_register_form');
+    $links .= '<li><a href="#" data-toggle="modal" data-target="#register-modal">' . t('Register') . '</a></li>';
+    $register_modal = '
+      <div class="modal fade" id="register-modal" role="dialog" aria-labelledby="modalRegisterLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">' . t('Close') . '</span></button>
+              <h4 class="modal-title" id="modalRegisterLabel">' . t('Register') . '</h4>
+            </div>
+      <div class="modal-body">
+      ' . drupal_render($register_form)  . '
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">' . t('Close') . '</button>
+      </div>
+          </div>
+        </div>
+      </div>
+      ';
+  }
+  $links .= '</ul>';
+  // JS to move modals, this is needed so modals do not appear under overlay.
+  drupal_add_js(
+  'Drupal.behaviors.bootstrap_login_modal = {
+      attach: function (context) {
+        jQuery("#login-modal", context).appendTo("body");
+        jQuery("#register-modal", context).appendTo("body");
+      }
+  };',
+  'inline');
+  return $links . $login_modal;
+}
