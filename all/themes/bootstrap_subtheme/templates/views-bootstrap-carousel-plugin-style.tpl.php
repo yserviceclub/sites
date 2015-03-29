ade5e422e3e67253f05b467ae1f36709d9e0fcef
plsg <patrick@patricklesage.ca>
2015-03-28 22:16:27 -0400

    Responsive styles and Newsletter page

---
+++ all/themes/bootstrap_subtheme/templates/views-bootstrap-carousel-plugin-style.tpl.php
@@ -0,0 +1,35 @@
+<?php if (!empty($title)): ?>
+  <h3><?php print $title ?></h3>
+<?php endif ?>
+
+<div id="views-bootstrap-carousel-<?php print $id ?>" class="<?php print $classes ?>" <?php print $attributes ?>>
+  <?php if ($indicators): ?>
+    <!-- Carousel indicators -->
+    <ol class="carousel-indicators">
+      <?php foreach ($rows as $key => $value): ?>
+        <li data-target="#views-bootstrap-carousel-<?php print $id ?>" data-slide-to="<?php print $key ?>" class="<?php if ($key == $first_key) print 'active' ?>"></li>
+      <?php endforeach ?>
+    </ol>
+  <?php endif ?>
+
+  <!-- Carousel items -->
+  <div class="carousel-inner">
+    <?php foreach ($rows as $key => $row): ?>
+      <div class="item <?php if ($key == $first_key) print 'active' ?>">
+        <?php print $row ?>
+      </div>
+    <?php endforeach ?>
+  </div>
+
+  <?php if ($navigation): ?>
+    <!-- Carousel navigation -->
+    <a class="carousel-control left" href="#views-bootstrap-carousel-<?php print $id ?>" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control right" href="#views-bootstrap-carousel-<?php print $id ?>" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  <?php endif ?>
+</div>
